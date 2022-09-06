05b60559a7a17e27b8d8823591a0fc26f92ba984
orkun <orkun@DESKTOP-GLG7E4J>
2022-09-06 13:44:54 +0300

    Add project files.

---
+++ DataAccess/Concreate/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess.Abstract;
+using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concreate.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+
+        List<Product> _products;
+
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+                new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
+                new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+                new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitsInStock=1}
+            };
+        }
+
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
